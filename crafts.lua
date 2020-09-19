b6fb1948e868f90c95cf77a52974a7d4819ada41
TenPlus1 <kinsellaja@yahoo.com>
2020-09-19 15:17:38 +0100

    meatblocks can be eaten for 20 hp

--- crafts.lua
+++ crafts.lua
@@ -340,7 +340,8 @@ minetest.register_node("mobs:meatblock", {
 	paramtype2 = "facedir",
 	groups = {choppy = 1, oddly_breakable_by_hand = 1, flammable = 2},
 	sounds = default.node_sound_leaves_defaults(),
-	on_place = minetest.rotate_node
+	on_place = minetest.rotate_node,
+	on_use = minetest.item_eat(20),
 })
 
 minetest.register_craft({
