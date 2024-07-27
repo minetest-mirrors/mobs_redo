ef9f492badf5c883b4c684a0173e52838d31d4a2
tenplus1 <kinsellaja@yahoo.com>
2024-07-27 18:28:44 +0100

    update {eatable} function

--- crafts.lua
+++ crafts.lua
@@ -12,17 +12,17 @@ function mobs.add_eatable(item, hp)
 
 	if def then
 
-		local grps = def.groups or {}
+		local groups = table.copy(def.groups) or {}
 		local txt = " (" ; if hp > 0 then txt = txt .. "+" end
 		txt = txt .. hp .. " HP)"
 
-		grps.eatable = hp ; grps.flammable = 2
+		groups.eatable = hp ; groups.flammable = 2
 
 		if mod_tt_base == nil then
 			def.description = def.description .. txt
 		end
 
-		minetest.override_item(item, {description = def.description, groups = grps})
+		minetest.override_item(item, {description = def.description, groups = groups})
 	end
 end
 
